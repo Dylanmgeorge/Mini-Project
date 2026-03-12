86083be61bec9893f31379a447a6d58ec77aa28d
dylanmgeorge <dylanmgeorge9@gmail.com>
2026-03-11 17:58:53 -0700

    Forgot to save before committing, this goes with previous change.

--- scripts/Raw-to-Processed_Script.R
+++ scripts/Raw-to-Processed_Script.R
@@ -5,13 +5,16 @@ library(tidyverse)
 
 #Load dataset#
 
-penguins_raw_dataset <- read.csv(here::here("data", "data_raw", "Penguins_Raw-Data.csv"))
+Adelie_Penguin_raw_dataset <- read.csv(here::here("data", "data_raw", "Adelie_Penguin_raw_data.csv"))
 
 #Transform data into processed data#
 
-Adelie_dataset <- penguins_raw_dataset %>%
-  filter(species == "Adelie")
+Adelie_Penguin_processed_data <- Adelie_Penguin_raw_dataset %>%
+  filter(Sex == "FEMALE")%>%
+  select(Species, Region, Individual.ID, Island, Flipper.Length..mm., Body.Mass..g., Sex)%>%
+  rename(Individual_ID = Individual.ID, Flipper_Length_mm = Flipper.Length..mm., Body_mass_g = Body.Mass..g.)
 
 #Write(save) to Processed data#
 
-write.csv(Adelie_dataset, here("data", "data_processed", "Adelie_dataset.csv"))
+write.csv(Adelie_Penguin_processed_data, here("data", "data_processed", "Adelie_Penguin_Processed_data.csv"))
+
